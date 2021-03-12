3f17fc898e542cf48bb2b2eef42a2ac7c4b03441
apsrivastava141 <srivastavaadamya@gmail.com>
2021-03-12 11:15:51 -0800

    ran a couple more embedding models, added more text examples, ran named location detection

--- infersent_consine_simalirity/spacy_and_bert_cosine_similarity.py
+++ infersent_consine_simalirity/spacy_and_bert_cosine_similarity.py
@@ -1,6 +1,7 @@
 from test import compute_train_test_split
 import spacy
 # python -m spacy download en_core_web_lg
+
 nlp = spacy.load("en_core_web_lg")
 import sklearn.linear_model as lm
 import sklearn.svm as svm
@@ -8,11 +9,7 @@ import sklearn.svm as svm
 def spacy_is_trigger(s, t):
     s = nlp(s)
     t = nlp(t)
-    return s.similarity(t) > 0.92
-    # 4 / 50 false positives
-    # 46 / 50 true negatives
-    # 13 / 14 true positives
-    # 1 / 14 false negatives
+    return s.similarity(t) > 0.93
 
 compute_train_test_split(spacy_is_trigger)
 
@@ -20,16 +17,27 @@ print()
 print()
 
 from sentence_transformers import SentenceTransformer, util
-model = SentenceTransformer('stsb-bert-large')
-def bert_large_is_trigger(s, t):
+model = SentenceTransformer('stsb-distilbert-base')
+def stsb_distilbert_is_trigger(s, t):
     s = model.encode(s)
     t = model.encode(t)
     cosine_score = util.pytorch_cos_sim(s, t)
-    return cosine_score > 0.8
-    # 0 / 50 false positives
-    # 50 / 50 true negatives
-    # 13 / 14 true positives
-    # 1 / 14 false negatives
+    return cosine_score > 0.73
 
 
-compute_train_test_split(bert_large_is_trigger)
+compute_train_test_split(stsb_distilbert_is_trigger)
+
+
+
+print()
+print()
+
+model = SentenceTransformer('nli-distilbert-base')
+def nli_distilbert_is_trigger(s, t):
+    s = model.encode(s)
+    t = model.encode(t)
+    cosine_score = util.pytorch_cos_sim(s, t)
+    return cosine_score > 0.75
+
+
+compute_train_test_split(nli_distilbert_is_trigger)
